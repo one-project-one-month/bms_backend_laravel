65f54c66ceb7f3f999ffee684c5a515376b964ff
YuzanaKyaw <yuzanakyaw160@gmail.com>
2024-05-11 12:50:32 +0630

    migrate state and township data

---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\State;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $jsonData = Storage::json('public/State.json');
+        // $states = json_decode($jsonData,true);
+
+        foreach($jsonData as $state){
+            State::query()->updateOrCreate([
+                // 'StateId' => $state['StateId'],
+                'StateCode' => $state['StateCode'],
+                'StateName' => $state['StateName']
+            ]);
+        }
+    }
+}
